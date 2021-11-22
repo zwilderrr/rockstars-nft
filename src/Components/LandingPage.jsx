b40dd06b307fc0e5d63e3d8182f697cac11be8bb
Zack Wilder <zwilder@gmail.com>
2021-11-22 10:43:45 -0500

    visibility

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -4,16 +4,69 @@ import multicolor from "../images/multicolor.png";
 import heat from "../images/heat.png";
 import { ctaText, rareText, whyText } from "../content";
 import { MintButton } from "./MintButton";
+import { useEffect, useRef, useState } from "react";
 
-function RockstarImage({ src }) {
+const useOnScreen = (ref, rootMargin = "0px") => {
+	const [isVisible, setIsVisible] = useState(false);
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			([entry]) => {
+				setIsVisible(entry.isIntersecting);
+			},
+			{
+				rootMargin,
+			}
+		);
+
+		const currentElement = ref?.current;
+
+		if (currentElement) {
+			observer.observe(currentElement);
+		}
+
+		return () => {
+			observer.unobserve(currentElement);
+		};
+	}, []);
+
+	return isVisible;
+};
+
+function RockstarImage({ src, isVisible = true }) {
 	return (
-		<div className="fadeIn">
+		<div className={`${isVisible ? "fadeIn" : "not-visible"}`}>
 			<img src={src} alt="rockstar" width="100%" />
 		</div>
 	);
 }
 
 export default function LandingPage({ web3, provider, Contract, isMobile }) {
+	const ref1 = useRef(null);
+	const ref2 = useRef(null);
+	const isVisible1 = useOnScreen(ref1);
+	const isVisible2 = useOnScreen(ref2);
+
+	function getHelperText() {
+		if (isMobile) {
+			return (
+				<div className="fadeIn" style={{ animationDelay: "2.6s" }}>
+					On mobile? Go to rockstars.buzz on the{" "}
+					<a
+						href="https://metamask.io/download"
+						className="external-link"
+						alt="metamask download"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Metamask
+					</a>{" "}
+					app browser
+				</div>
+			);
+		}
+	}
+
 	return (
 		<div>
 			<div className="first">
@@ -39,6 +92,7 @@ export default function LandingPage({ web3, provider, Contract, isMobile }) {
 
 					<div className="col-right">
 						{!isMobile && <RockstarImage src={rockstarMain} />}
+						{getHelperText()}
 					</div>
 				</div>
 			</div>
@@ -46,16 +100,16 @@ export default function LandingPage({ web3, provider, Contract, isMobile }) {
 			<div className="bgwhite">
 				<div className="row">
 					<div className="col-right">
-						<RockstarImage src={heat} />
+						<RockstarImage src={heat} isVisible={isVisible1} />
 					</div>
 					<div className="spacer" />
-					<div className="col-left">
+					<div className="col-left" ref={ref1}>
 						<div className="cta-text-wrapper">
 							{whyText.map((line, i) => (
 								<div
 									key={i}
 									style={{ animationDelay: `${0.5 + 0.6 * i}s` }}
-									className="rare-text fadeInUp"
+									className={`rare-text ${isVisible1 ? "fadeInUp" : ""}`}
 								>
 									{line}
 								</div>
@@ -68,13 +122,13 @@ export default function LandingPage({ web3, provider, Contract, isMobile }) {
 			<div>
 				<div className="row">
 					{isMobile && <RockstarImage src={multicolor} />}
-					<div className="col-left">
+					<div className="col-left" ref={ref2}>
 						<div className="cta-text-wrapper">
 							{rareText.map((line, i) => (
 								<div
 									key={i}
 									style={{ animationDelay: `${0.5 + 0.6 * i}s` }}
-									className="rare-text fadeInUp"
+									className={`rare-text ${isVisible2 ? "fadeInUp" : ""}`}
 								>
 									{line}
 								</div>
