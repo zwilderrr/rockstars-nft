908039e826655e00af33fe288f4bd25f22e7a367
zack wilder <zack.wilder@coinbase.com>
2022-01-31 16:20:10 -0500

    copy

--- src/content.jsx
+++ src/content.jsx
@@ -118,7 +118,10 @@ export const timelineText = [
 
 export const rareText = [
 	"When you mint a Rockstar, you mint a vote for humanity. You proudly state that life is not a zero-sum game. It's a game of big and little triumphs, and big and little acts of kindness, and it is only won when everyone wins.",
-	"When you're a Rockstar, you're a good person living an epic life—even if epic just means treating the local barista really nicely.",
+	<span>
+		When you <i>are</i> a Rockstar, you're a good person living an epic
+		life—even if epic just means treating the local barista really nicely.
+	</span>,
 ];
 // "Life isn't a choice between being rich and powerful but not a nice person doing not nice things, or being kind and giving but feeling small and unimportant.",
 
@@ -141,7 +144,7 @@ export const rareText = [
 export const whyText = [
 	"That's the ethos we wanted to bring to the world, and that's why we created RockstarsNFT. They're Rockstars, so they live big, but they're cartoons, so they're fun and friendly (and even a bit mischievous!). ",
 	"Rare, ultra-rare, and hyper-rare Rockstars are ready to rock and roll.",
-	"Mint one now and live the dream.",
+	// "Mint one now and live the dream.",
 ];
 
 // export const whyText = "Gain instant access to a community of epic people living epic lives.",
