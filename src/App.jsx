2eaa22e53ca0028068ccffa65b5959ccbf2bedee
zack wilder <zack.wilder@coinbase.com>
2022-04-04 11:29:12 -0400

    always show shrunken header on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ export default function App() {
 				setWeb3={setWeb3}
 				setProvider={setProvider}
 				setContract={setContract}
-				shrink={isMobile ? false : shrinkHeader}
+				shrink={isMobile ? true : shrinkHeader}
 				isMobile={isMobile}
 			/>
 
